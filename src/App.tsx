2322499c8c874002a4119e75ae22797f25ca694b
alexfcode <alexf@tut.by>
2024-12-01 15:44:58 +0300

    testimony - done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
+import { Works } from './layout/sections/works/Works';
+import { Testimony } from './layout/sections/testimony/Testimony';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
             <Header/>
             <Main />
             <Skills/>
+            <Works />
+            <Testimony />
         </div>
     );
 }
---
+++ src/layout/sections/works/Works.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import { SectionTitle } from "../../../components/SectionTitle";
+import { Menu } from "../../../components/menu/Menu";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+import { Work } from "./work/Work";
+import socialImg from "../../../assets/images/Proj-1.webp"
+import timerImg from "../../../assets/images/Proj-2.webp"
+
+const worksItems = ["Home", "Landing Page", "Reacp", "SPA",]
+
+export const Works = () => {
+    return (
+        <StyledWorks>
+            <SectionTitle>My Works</SectionTitle>
+            <Menu menuItems={worksItems} />
+            <FlexWrapper justify={"space-around"}>
+                <Work 
+                src={socialImg} 
+                title={"Social Network"} 
+                text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                <Work 
+                src={timerImg} 
+                title={"Timer"} 
+                text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}/>
+            </FlexWrapper>
+        </StyledWorks>
+    );
+};
+
+const StyledWorks = styled.section`
+    min-height: 100vh;
+    background-color: #f3f7c5;
+`
