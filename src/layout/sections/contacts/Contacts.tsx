5baddde1624561ec748cd0b9029dd2833de476f8
alexf <alexf@tut.by>
2024-12-19 17:37:25 +0300

    Portfolio is finished

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -6,8 +6,7 @@ import { S } from "./Contact_Styles";
 import emailjs from "@emailjs/browser";
 
 export const Contacts: React.FC = () => {
-    const form = useRef<ElementRef<"form">>(null);
-    
+  const form = useRef<ElementRef<"form">>(null);
   const sendEmail = (e: any) => {
     e.preventDefault();
 
@@ -25,17 +24,20 @@ export const Contacts: React.FC = () => {
           console.log("FAILED...", error.text);
         }
       );
+      e.target.reset();
   };
   return (
     <S.Contacts id="contact">
       <Container>
         <SectionTitle>Contact</SectionTitle>
         <S.Form ref={form} onSubmit={sendEmail}>
-          <S.Field placeholder={"name"} name={"username"}></S.Field>
-          <S.Field placeholder={"subject"} name={"subject"}></S.Field>
+          <S.Field required placeholder={"name"} name={"username"}></S.Field>
+          <S.Field required placeholder={"email"} name={"email"}></S.Field>
+          <S.Field required placeholder={"subject"} name={"subject"}></S.Field>
           <S.Field
             placeholder={"type your message..."}
-            as={"textarea"} name={"message"}
+            as={"textarea"}
+            name={"message"}
           ></S.Field>
           <Button type={"submit"}>Send Message</Button>
         </S.Form>
